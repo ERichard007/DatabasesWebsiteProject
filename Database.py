9461b9fa31bffafb2888e12aee22f5ed54f6c4f2
Evan <ecrb29@umsystem.edu>
2025-11-16 18:19:38 -0600

    Alright so this marks the spot where I start working on the character creation page...

--- Database.py
+++ Database.py
@@ -3,7 +3,8 @@ import sqlite3
 connection = sqlite3.connect('DnDCharacterManager.db')
 cursor = connection.cursor()
 
-ResetTables = """DROP TABLE IF EXISTS User"""
+ResetTable = """DROP TABLE IF EXISTS Character"""
+ResetTable2 = """DROP TABLE IF EXISTS User"""
 
 UserTable = """CREATE TABLE IF NOT EXISTS
 User(
@@ -12,8 +13,27 @@ username TEXT UNIQUE NOT NULL,
 passw TEXT NOT NULL
 )"""
 
-cursor.execute(ResetTables)
+CharacterTable = """CREATE TABLE IF NOT EXISTS
+Character(
+characterid INTEGER PRIMARY KEY AUTOINCREMENT,
+userid INTEGER NOT NULL REFERENCES User(userid),
+background TEXT NOT NULL,
+iscompanion INTEGER NOT NULL CHECK(iscompanion IN (0, 1)) DEFAULT 0, 
+name TEXT,
+playername TEXT,
+electrum INTEGER CHECK(electrum >= 0) DEFAULT 0,
+gold INTEGER CHECK(gold >= 0) DEFAULT 0,
+silver INTEGER CHECK(silver >= 0) DEFAULT 0,
+copper INTEGER CHECK(copper >= 0) DEFAULT 0,
+platinum INTEGER CHECK(platinum >= 0) DEFAULT 0,
+ownerid INTEGER REFERENCES Character(characterid) DEFAULT NULL
+)"""
+
+cursor.execute("PRAGMA foreign_keys = ON;")
+cursor.execute(ResetTable)
+cursor.execute(ResetTable2)
 cursor.execute(UserTable)
+cursor.execute(CharacterTable)
 
 connection.commit()
 connection.close()
--- MyFlask.py
+++ MyFlask.py
@@ -1,14 +1,15 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session
 from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 
 app = Flask(__name__)
+app.secret_key = "dev12345" ## NEED TO CHANGE LATER (IMPORTANT!!!)
 
-@app.route("/")
+@app.route("/") #default page
 def index():
     return render_template("index.html")
     
-@app.route("/login", methods=["GET","POST"])
+@app.route("/login", methods=["GET","POST"]) #login page
 def login():
     if request.method == "POST":
         user = request.form["username"]
@@ -16,20 +17,21 @@ def login():
 
         conn = sqlite3.connect('DnDCharacterManager.db')
         cursor = conn.cursor()
-        cursor.execute("SELECT passw FROM User WHERE username = ?", (user,))
+        cursor.execute("SELECT passw, userid FROM User WHERE username = ?", (user,))
         row = cursor.fetchone()
 
         conn.close()
 
         if row and check_password_hash(row[0], passw):
-            return "login succesfull!"
+            session["userid"] = row[1]
+            return redirect("/list")
         else:
-            return "unsuccesfull login"
+            return "Incoreect username or password"
             
 
     return render_template("login.html")
 
-@app.route("/register", methods=["GET","POST"])
+@app.route("/register", methods=["GET","POST"]) #registeration page
 def register():
     if request.method == "POST":
         user = request.form["username"]
@@ -41,17 +43,11 @@ def register():
 
         row = cursor.fetchone()
 
-        cursor.execute("SELECT * FROM User")
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
-
         try:
             hashp = generate_password_hash(passw)
             cursor.execute("INSERT INTO User(username, passw) VALUES(?,?)", (user, hashp,))
             conn.commit()
             conn.close()
-
             return render_template("login.html")
 
         except sqlite3.IntegrityError:
@@ -59,6 +55,37 @@ def register():
             return "username already taken!"
 
     return render_template("register.html")
+
+@app.route("/list") #character listing page
+def list_characters():
+    conn = sqlite3.connect("DnDCharacterManager.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM User")
+    print(cursor.fetchall())
+    cursor.execute("SELECT * FROM Character")
+    print(cursor.fetchall())
+
+    user_id = session.get("userid")
+
+    cursor.execute("SELECT characterid, name, background FROM Character WHERE userid = ?", (user_id,))
+    characters = cursor.fetchall()
+
+    cursor.execute("SELECT username FROM User WHERE userid = ?", (user_id,))
+    username = cursor.fetchone()[0]
+
+    conn.close()
+
+    return render_template("list.html", characters=characters, username=username)
+
+@app.route("/character/<int:cid>") #character editing page
+def character(cid):
+    return render_template("character.html")
+
+@app.route("/charactercreation", methods=["GET","POST"]) #character creation page
+def create_character():
+    
+    return render_template("charactercreation.html")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=True)
